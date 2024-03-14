eb8b72caf02a24c6713584727bd1e1888c8294a8
Jack W <jackweber943@gmail.com>
2024-03-14 12:59:26 -0400

    ik its shit rn but i have to study bio

--- main.cpp
+++ main.cpp
@@ -1,64 +1,117 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int encrypt(string message){     //encrypts the message
-    string lettersandnumbers [26][2]= {
-    {"a", "1"},
-    {"b", "2"},
-    {"c", "3"},
-    {"d", "4"},
-    {"e", "5"},
-    {"f", "6"},
-    {"g", "7"},
-    {"h", "8"},
-    {"i", "9"},
-    {"j", "10"},
-    {"k", "11"},
-    {"l", "12"},
-    {"m", "13"},
-    {"n", "14"},
-    {"o", "15"},
-    {"p", "16"},
-    {"q", "17"},
-    {"r", "18"},
-    {"s", "19"},
-    {"t", "20"},
-    {"u", "21"},
-    {"v", "22"},
-    {"w", "23"},
-    {"x", "24"},
-    {"y", "25"},
-    {"z", "26"},
-    };
+void encrypt(){
+    string to_encrypt;
+    cout << "Enter the string to encrypt (no special characters or numbers, only letters): ";
+    cin >> to_encrypt;
+    int encryptedint [to_encrypt.length()][2];
+    int i = 0;
+    while(i < to_encrypt.length()){
+        if (to_encrypt[i] == 'a'){
+            encryptedint [i] = 1;
+        }
+        else if (to_encrypt[i] == 'b'){
+            encryptedint[i] = 2;
+        }
+        else if (to_encrypt[i] == 'c'){
+            encryptedint[i] = 3;
+        }
+        else if (to_encrypt[i] == 'd'){
+            encryptedint[i] = 4;
+        }
+        else if (to_encrypt[i] == 'e'){
+            encryptedint[i] = 5;
+        }
+        else if (to_encrypt[i] == 'f'){
+            encryptedint[i] = 6;
+        }
+        else if (to_encrypt[i] == 'g'){
+           encryptedint[i] = 7;
+        }
+        else if (to_encrypt[i] == 'h'){
+           encryptedint[i] = 8;
+        }
+        else if (to_encrypt[i] == 'i'){
+           encryptedint[i] = 9;
+        }
+        else if (to_encrypt[i] == 'j'){
+          encryptedint[i] = 10;
+        }
+        else if (to_encrypt[i] == 'k'){
+           encryptedint[i] = 11;
+        }
+        else if (to_encrypt[i] == 'l'){
+           encryptedint[i] = 12;
+        }
+        else if (to_encrypt[i] == 'm'){
+           encryptedint[i] = 13;
+        }
+        else if (to_encrypt[i] == 'n'){
+           encryptedint[i] = 14;
+        }
+        else if (to_encrypt[i] == 'o'){
+           encryptedint[i] = 15;
+        }
+        else if (to_encrypt[i] == 'p'){
+            encryptedint[i] = 16;
+        }
+        else if (to_encrypt[i] == 'q'){
+            encryptedint[i] = 17;
+        }
+        else if (to_encrypt[i] == 'r'){
+           encryptedint[i] = 18;
+        }
+        else if (to_encrypt[i] == 's'){
+            encryptedint[i] = 19;
+        }
+        else if (to_encrypt[i] == 't'){
+            encryptedint[i] = 20;
+        }
+        else if (to_encrypt[i] == 'u'){
+           encryptedint[i] = 21;
+        }
+        else if (to_encrypt[i] == 'v'){
+            encryptedint[i] = 22;
+        }
+        else if (to_encrypt[i] == 'w'){
+            encryptedint[i] = 23;
+        }
+        else if (to_encrypt[i] == 'x'){
+           encryptedint[i] = 24;
+        }
+        else if (to_encrypt[i] == 'y'){
+           encryptedint[i] = 25;
+        }
+        else if (to_encrypt[i] == 'z'){
+           encryptedint[i] = 26;
+        }
+        i++;
+    }
+    cout << to_encrypt << endl;
+}
 
-};
-
-
-string decrypt(string message){        //decrypts the message
-    //placeholder
+void decrypt(){
+    string to_decrypt;
+    cout << "Enter the string to decrypt: ";
+    cin >> to_decrypt;
+    cout << to_decrypt << endl;
 }
 
 
-int main(){                     //handles user interaction
-    string initial_option;
-    cout << "Encrypt or decrypt? ";
-    cin >> initial_option;
-    if (initial_option == "encrypt"){
-        string message;
-        cout << "Enter the message you want to encrypt: ";
-        cin >> message;
-        cout << "Encrypted version: " << encrypt(message) << endl;
-    }
 
-    else if (initial_option == "decrypt"){
-        string message;
-        cout << "Enter the key: ";
-        cin >> message;
-        cout << "Decrypted version: " << decrypt(message) << endl;
+int main(){
+    string firstanswer;
+    cout << "Encrypt or decrypt: ";
+    cin >> firstanswer;
+    if (firstanswer == "encrypt"){
+        encrypt();
     }
-
+    else if (firstanswer == "decrypt"){
+        decrypt();
+    }
     else{
-        cout << "Invalid option" << endl;
+        cout << "Invalid input" << endl;
     }
 }
